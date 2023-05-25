2cb7fed8c2d8ae42f3e5946504b3783246de3c8f
Jeff Cheng <jeffcheng777@gmail.com>
2023-05-25 13:43:02 -0400

    Daniel is an indian

--- tetetetets.py
+++ tetetetets.py
@@ -1,19 +1,34 @@
 import json
-import logging    
+import logging
 
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)  # You can set this to be DEBUG, INFO, ERROR as needed
+class CustomLogger(logging.Logger):
+    def info(self, msg, *args, **kwargs):
+        extra = kwargs.get("extra", {})
+        request_id = extra.get("request_id", "")
+        taskId = extra.get("taskId", "")
+        state = extra.get("state", "")
+        extra.update({"request_id": request_id, "taskId": taskId, "state": state})
+        super().info(msg, *args, extra=extra)
+
+
+logger = CustomLogger()
+logger.setLevel(logging.INFO)
+
 
 def log_format(func):
     def wrapper(*args, **kwargs):
-        formatter = logging.Formatter('[%(levelname)s]\t%(asctime)s\t%(request_id)s\tPID:%(taskId)s  STATE:%(state)s  MESSAGE:%(message)s','%Y-%m-%dT%H:%M:%S')
+        formatter = logging.Formatter(
+            "[%(levelname)s]\t%(asctime)s\t%(request_id)s\tPID:%(taskId)s  STATE:%(state)s  MESSAGE:%(message)s",
+            "%Y-%m-%dT%H:%M:%S",
+        )
         for handler in logger.handlers:
             logger.removeHandler(handler)
         ch = logging.StreamHandler()
         ch.setFormatter(formatter)
         logger.addHandler(ch)
         func(*args, **kwargs)
+
     return wrapper
 
 
@@ -22,9 +37,9 @@ def lambda_handler(event, context):
     request_id = context.aws_request_id
 
     # Include the request ID and custom inputs in logging calls
-    taskId = 1222333
-    state = "RUNNING"
-    extra={'request_id': request_id, 'taskId': taskId, 'state': state}
-    logger.info('This is an info log message', extra=extra)
-    logger.info('hi',extra=extra) 
-    logger.error('danger danger',extra=extra)
+    # taskId = 1222333
+    # state = "RUNNING"
+    # extra = {"request_id": request_id, "taskId": taskId, "state": state}
+    logger.info("This is an info log message")
+    logger.info("hi")
+    logger.error("danger danger")
