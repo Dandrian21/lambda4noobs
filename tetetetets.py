f2bf9421067ea7c8ac73ff21e41d02793adaaaf5
Jeff Cheng <jeffcheng777@gmail.com>
2023-05-25 22:48:25 -0400

    added a add_extra function

--- tetetetets.py
+++ tetetetets.py
@@ -1,45 +1,39 @@
 import json
-import logging
+import logging    
 
-
-class CustomLogger(logging.Logger):
-    def info(self, msg, *args, **kwargs):
-        extra = kwargs.get("extra", {})
-        request_id = extra.get("request_id", "")
-        taskId = extra.get("taskId", "")
-        state = extra.get("state", "")
-        extra.update({"request_id": request_id, "taskId": taskId, "state": state})
-        super().info(msg, *args, extra=extra)
-
-
-logger = CustomLogger()
-logger.setLevel(logging.INFO)
-
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)  # You can set this to be DEBUG, INFO, ERROR as needed
 
 def log_format(func):
     def wrapper(*args, **kwargs):
-        formatter = logging.Formatter(
-            "[%(levelname)s]\t%(asctime)s\t%(request_id)s\tPID:%(taskId)s  STATE:%(state)s  MESSAGE:%(message)s",
-            "%Y-%m-%dT%H:%M:%S",
-        )
+        formatter = logging.Formatter('[%(levelname)s]\t%(asctime)s\t%(request_id)s\tPID:%(taskId)s  STATE:%(state)s  MESSAGE:%(message)s','%Y-%m-%dT%H:%M:%S')
         for handler in logger.handlers:
             logger.removeHandler(handler)
         ch = logging.StreamHandler()
         ch.setFormatter(formatter)
         logger.addHandler(ch)
+        logger.info = add_extra(logger.info)  # Override logger.info with the decorated version
         func(*args, **kwargs)
-
     return wrapper
 
+def add_extra(logger_info):
+    def wrapper(msg, *args, **kwargs):
+        extra = kwargs.get('extra', {})
+        request_id = extra.get('request_id')
+        taskId = 1222333
+        state = "RUNNING"
+        updated_extra = {'request_id': request_id, 'taskId': taskId, 'state': state}
+        updated_extra.update(extra)  # Merge existing extra with updated_extra
+        kwargs['extra'] = updated_extra
+        return logger_info(msg, *args, **kwargs)
+    return wrapper
 
 @log_format
 def lambda_handler(event, context):
     request_id = context.aws_request_id
 
-    # Include the request ID and custom inputs in logging calls
-    # taskId = 1222333
-    # state = "RUNNING"
-    # extra = {"request_id": request_id, "taskId": taskId, "state": state}
-    logger.info("This is an info log message")
-    logger.info("hi")
-    logger.error("danger danger")
+    logger.info('This is an info log message')
+    logger.info('hi')
+    logger.error('danger danger')
+
+lambda_handler(None, None)  # Example invocation
